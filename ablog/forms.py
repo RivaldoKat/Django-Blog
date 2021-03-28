3e9b139da1719ca5de898b5fec46f72a72d75087
RivaldoKat <rivaldokat25@gmail.com>
2021-03-28 11:14:03 +0200

    A blog Website
    A simple django blog

---
+++ ablog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = '__all__'
+
+		widgets = { 'title' : forms.TextInput(attrs={'class': 'form-control'}),
+					'title_tag' : forms.TextInput(attrs={'class': 'form-control'}),
+					'author' : forms.Select(attrs={'class': 'form-control'}),
+					'body' : forms.Textarea(attrs={'class': 'form-control'}),
+	}
+
+class EditForm(forms.ModelForm):
+	class Meta:
+		model = Post
+		fields = '__all__'
+
+		widgets = { 'title' : forms.TextInput(attrs={'class': 'form-control'}),
+					'title_tag' : forms.TextInput(attrs={'class': 'form-control'}),
+					'author' : forms.Select(attrs={'class': 'form-control'}),
+					'body' : forms.Textarea(attrs={'class': 'form-control'}),
+					}
