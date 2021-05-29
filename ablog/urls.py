bcd42a7d8db5280ec97126d139f3863bb86d373e
RivaldoKat <rivaldokat25@gmail.com>
2021-05-29 08:46:43 +0200

    Updates

--- ablog/urls.py
+++ ablog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from .import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView, LikeView
 
 urlpatterns = [
     # path('', views.home, name="home"),
@@ -8,5 +8,9 @@ urlpatterns = [
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 	path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),   
     path('add_post/', AddPostView.as_view(), name='add_post'),
-    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post',)
+    path('add_category/', AddCategoryView.as_view(), name='add_category'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
+    path('category/<str:category>/', CategoryView, name='category'),
+    path('category-list/', CategoryListView, name='category-list'),
+    path('like/<int:pk>', LikeView, name = 'like_post'),
 ]	
