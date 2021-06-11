b91d1965c61d4443c80feeb45c29edfa7192b470
RivaldoKat <70294721+RivaldoKat@users.noreply.github.com>
2021-06-11 19:29:04 +0200

    updating the previous error

--- ablog/urls.py
+++ ablog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 #from .import views
+from django.contrib.auth import views as auth_views
 from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView, LikeView
 
 urlpatterns = [
@@ -13,4 +14,9 @@ urlpatterns = [
     path('category/<str:category>/', CategoryView, name='category'),
     path('category-list/', CategoryListView, name='category-list'),
     path('like/<int:pk>', LikeView, name = 'like_post'),
+
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]	
--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&3=jx-0sq2!d*@wi%-mw-ab3!@8pgye*qybzxhc@jm1waul_c&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.28']
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ablog',
     'members',
-    'ckeditor'
+    
 ]
 
 MIDDLEWARE = [
